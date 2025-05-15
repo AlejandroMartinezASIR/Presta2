016a64364c841d7b300a02c6728e19396d85bc0b
John Doe <johndoe@example.com>
2025-05-15 11:16:54 +0000

    dddddddd

--- install_lamp_backend.sh
+++ install_lamp_backend.sh
@@ -18,7 +18,7 @@ sudo apt install mariadb-server -y
 # Copiamos el archivo de configuracion de mysql
 sed -i "s/^bind-address\s*=.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# Borramos la base de datos y el usuario si existen
+# Borramos la base de datos y los usuarios si existen
 mysql -u root <<EOF
 DROP DATABASE IF EXISTS prestashop;
 DROP USER IF EXISTS 'psuser'@'localhost';
@@ -26,13 +26,14 @@ DROP USER IF EXISTS 'psuser'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-# Creamos la base de datos y el usuario para PRESTASHOP
+# Creamos la base de datos y los usuarios para PRESTASHOP
 mysql -u root <<EOF
 CREATE DATABASE prestashop CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER 'psuser'@'localhost' IDENTIFIED BY 'pspass';
+CREATE USER IF NOT EXISTS 'psuser'@'localhost' IDENTIFIED BY 'pspass';
+CREATE USER IF NOT EXISTS 'psuser'@'%' IDENTIFIED BY 'pspass';
+GRANT ALL PRIVILEGES ON prestashop.* TO 'psuser'@'localhost';
 GRANT ALL PRIVILEGES ON prestashop.* TO 'psuser'@'%';
 FLUSH PRIVILEGES;
 EOF
-
 # Reiniciamos el servicio de MySQL
 systemctl restart mariadb
