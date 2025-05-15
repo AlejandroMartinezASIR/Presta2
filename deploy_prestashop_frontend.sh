c9ae23bc1a670429411cd48b6f74adc77b6670c4
John Doe <johndoe@example.com>
2025-05-15 09:25:58 +0000

    miau

--- deploy_prestashop_frontend.sh
+++ deploy_prestashop_frontend.sh
@@ -5,3 +5,24 @@ set -ex
 
 #Importamos las variables de entorno
 source .env
+
+# Instalamos el servidor LAMP
+cd /tmp
+
+# Descargamos la última versión de Prestashop
+wget https://assets.prestashop3.com/dst/edition/corporate/8.2.1/prestashop_edition_classic_version_8.2.1.zip
+
+# Descomprimimos el archivo
+unzip prestashop_edition_classic_version_8.2.1.zip -d prestashop
+
+# Eliminamos instalaciones anteriores
+sudo rm -rf /var/www/html/*
+
+# Movemos los archivos a la carpeta de Apache
+sudo mv prestashop/* /var/www/html/
+
+# Cambiamos los permisos de la carpeta
+sudo chown -R www-data:www-data /var/www/html/
+
+# Cambiamos los permisos de la carpeta
+sudo chmod -R 755 /var/www/html/
