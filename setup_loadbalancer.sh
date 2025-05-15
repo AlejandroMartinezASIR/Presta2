2a6767cfc1bd491a5482b201a66c980f5609011b
John Doe <johndoe@example.com>
2025-05-15 10:02:21 +0000

    jj

--- setup_loadbalancer.sh
+++ setup_loadbalancer.sh
@@ -15,12 +15,19 @@ apt upgrade -y
 #Instalamos nginx
 apt install nginx -y
 
+# Instalamos dependencias de Nginx
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update
+sudo apt install php7.4 php7.4-cli  php7.4-xml php7.4-curl php7.4-gd php7.4-mbstring php7.4-zip php7.4-intl -y
+
+
 # Deshabilitamos el virtualhost por defecto
 if [ -f "/etc/nginx/sites-enabled/default"]; then
 unlink /etc/nginx/sites-enabled/default
 fi
 # Copiamos el archivo de configuración de Nginx
-cp ../conf/load-balancer.conf /etc/nginx/sites-available
+cp /home/ubuntu/Presta2/conf/load-balancer.conf /etc/nginx/sites-available
 
 # Sustituimos los valores de la plantilla del archivo de configuración
 sed -i "s/IP_FRONTEND_1/$IP_FRONTEND_1/" /etc/nginx/sites-available/load-balancer.conf
